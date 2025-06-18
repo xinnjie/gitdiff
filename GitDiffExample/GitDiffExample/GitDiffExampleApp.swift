ecb6037ff0105477f5c7dd7aa51608b2c50e20d5
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-06-18 18:33:14 +0400

    root

---
+++ GitDiffExample/GitDiffExample/GitDiffExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  GitDiffExampleApp.swift
+//  GitDiffExample
+//
+//  Created by Tornike Gomareli on 18.06.25.
+//
+
+import SwiftUI
+
+@main
+struct GitDiffExampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      MainTabView()
+        .preferredColorScheme(.light)
+    }
+  }
+}
